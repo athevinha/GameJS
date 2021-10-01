9deb2390244b5a0b0a53c4bdbcd4e13aaacd0ef5
athevinha <nguyenthevinh1002@gmail.com>
2021-10-01 15:28:53 +0700

    new game

---
+++ tree_game/js/sky.js
@@ -0,0 +1,32 @@
+(function() {
+  function Sky(canvas, dropper) {
+    this.canvas = canvas;
+    this.dropper = dropper;
+    this.clouds = [];
+    this.addCloud('\n');
+  }
+
+  Sky.prototype.render = function(ctx) {
+    for (var i = 0; i < this.clouds.length; ++i)
+      this.clouds[i].render(ctx);
+  };
+
+  Sky.prototype.update = function(delta) {
+    var toDelete = [];
+    for (var i = 0; i < this.clouds.length; ++i) {
+      if (this.clouds[i].update(delta))
+        toDelete.push(this.clouds[i]);
+    }
+
+    this.clouds = _.difference(this.clouds, toDelete);
+
+    if (Math.random() < 0.002)
+      this.addCloud();
+  };
+
+  Sky.prototype.addCloud = function(text) {
+    this.clouds.push(new Cloud(this.canvas, this.dropper, text));
+  };
+
+  window.Sky = Sky;
+})();
